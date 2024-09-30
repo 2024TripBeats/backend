547e9af55c44d3ca03d701bcdaa256100ceb83a9
hunny7434 <55790304+hunny7434@users.noreply.github.com>
2024-09-30 23:14:03 +0900

    feat: add docker

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# 1. JDK 기반 이미지 사용 (Java 21)
+FROM openjdk:21-jdk-slim
+
+# 2. 작업 디렉토리 설정
+WORKDIR /app
+
+# 3. 로컬에서 빌드한 JAR 파일을 컨테이너로 복사
+COPY build/libs/TripBeatsBackend-0.0.1-SNAPSHOT.jar /app/your-application.jar
+
+# 4. CSV 파일을 resources 디렉토리로 복사
+COPY src/main/resources/flight_fare.csv /app/resources/flight_fare.csv
+COPY src/main/resources/place.csv /app/resources/place.csv
+
+# 5. JAR 파일 실행
+ENTRYPOINT ["java", "-jar", "/app/your-application.jar"]
--- src/test/java/team/tripbeats/backend/TripBeatsBackendApplicationTests.java
+++ src/test/java/team/tripbeats/backend/TripBeatsBackendApplicationTests.java
@@ -2,8 +2,16 @@ package team.tripbeats.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(
+        properties = {
+                "KAKAO_CLIENT_ID=example",
+                "KAKAO_REDIRECT_URI=example",
+                "KAKAO_CLIENT_SECRET=example"
+        }
+)
 class TripBeatsBackendApplicationTests {
 
     @Test
